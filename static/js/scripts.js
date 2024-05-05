8734b5086772917c2ff221fd5b6a8cde94f28387
Avezou Petit-Frere <github@apfware.com>
2024-05-05 03:02:39 -0400

    Filter by tag working. Need to add category to database and appropriate that filter.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,25 +1,15 @@
-// var myOptions = {
-//     val1 : 'Blue',
-//     val2 : 'Orange'
-// };
-// var mySelect = $('#myColors');
-// $.each(myOptions, function(val, text) {
-//     mySelect.append(
-//         $('<option></option>').val(val).html(text)
-//     );
-// });
 
-// var names = ["Bob Hope","James Jones","Steve Jobs","Larry McBridge"]   
-// var query = "bo"
-// var results = $(names)
-//         .map(function(i,v){ 
-//             if(v.toLowerCase().indexOf(query.toLowerCase())!=-1){return v} 
-//         }).get()
-$(document).ready(function(){
-    $("#mySearch").on("keyup", function() {
-      var value = $(this).val().toLowerCase();
-      $("#myList div").filter(function() {
-        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-      });
+$("#mySearch").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myList div").filter(function() {
+    $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
     });
-  });
+});
+
+$('select[id="myTags"]').change(function() {
+    var value = $(this).val().toLowerCase();
+    console.log("logging select: " +value);
+    $("#myList div").filter(function() {
+    $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+});
