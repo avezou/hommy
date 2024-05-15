f8e72649011aa911b6494381e9d3ae4fe2d6d928
Avezou Petit-Frere <avezou.petit-frere@caci.com>
2024-05-15 15:06:40 -0400

    Getting closer with edit. Need to fix tags.

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
-import sqlite3
-import requests
-import os
 import pathlib
-import urllib.request
-from itertools import groupby
-from flask import Flask, render_template, request, redirect, url_for
-from PIL import Image
+import sqlite3
+
+from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
+
 from config import Config
 from forms import AppForm
-from werkzeug.utils import secure_filename
-
 
 app = Flask(__name__, template_folder='templates')
 app.config.from_object(Config)
@@ -23,6 +18,7 @@ def get_db_connection():
     connect.row_factory = sqlite3.Row
     return connect
 
+
 def execute_query(query, args=(), one=False):
     connection = get_db_connection()
     cursor = connection.execute(query, args)
@@ -32,10 +28,10 @@ def execute_query(query, args=(), one=False):
     connection.close()
     return (rv[0] if rv else None) if one else rv
 
+
 def allowed_file(filename):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
-
+        filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
 
 def get_apps():
@@ -43,9 +39,10 @@ def get_apps():
                             FROM apps a ORDER BY a.category')
     if len(apps) == 0:
         return []
-    newapp = {}
+    new_app = {}
+    search_apps = ''
     for myapp in apps:
-        searchApps = myapp['name'].lower()
+        search_apps = myapp['name'].lower()
         tags = execute_query('SELECT a.id, a.tag\
                             FROM tags a \
                             JOIN app_tags at\
@@ -56,29 +53,31 @@ def get_apps():
                             JOIN categories c\
                             ON a.category = c.cat \
                             WHERE a.id = ?', (myapp['id'],))
-        
-        newapp[myapp] = tags
 
-    allTags = execute_query('SELECT a.id, a.tag\
+        new_app[myapp] = tags
+
+    all_tags = execute_query('SELECT a.id, a.tag\
                             FROM tags a ORDER BY a.tag')
-    allCategories = execute_query('SELECT c.cat\
+    all_categories = execute_query('SELECT c.cat\
                             FROM categories c ORDER BY c.cat')
-    return newapp, allTags, allCategories, searchApps
+    return new_app, all_tags, all_categories, search_apps
 
 
 @app.route('/')
 def index():
     res = get_apps()
     if len(res) <= 0:
-        return render_template('index.html')     
-    return render_template('index.html', apps=res[0], tags=res[1], categories=res[2], appNames=res[3]) 
+        return render_template('index.html')
+    return render_template('index.html', apps=res[0], tags=res[1], categories=res[2], appNames=res[3])
+
 
 @app.route('/list', methods=['GET', 'POST'])
 def list():
-    res =  get_apps()
+    res = get_apps()
     if len(res) <= 0:
-        return render_template('index.html')          
+        return render_template('index.html')
     return render_template('list.html', apps=res[0])
+
 
 @app.route('/delete/<int:id>')
 def delete(id):
@@ -107,7 +106,7 @@ def edit(id):
 
     if form.validate_on_submit():
         # Extract form data
-        appName = form.name.data
+        app_name = form.name.data
         category = form.category.data
         description = form.description.data
         internal_url = form.internal_url.data
@@ -115,13 +114,24 @@ def edit(id):
         extras = form.extras.data
         icon = form.icon.data
 
-
         # Process tags from the form
         tags = form.tags.data.split(',')
 
+        for tag in tags:
+            execute_query('INSERT OR IGNORE INTO tags (tag) VALUES (?)', (tag.strip(),))
+
         # Update app data in the database
-        execute_query('UPDATE apps SET name=?, category=?, description=?, internal_url=?, external_url=?, icon=?, extras=? WHERE id=?',
-                      (appName, category, description, internal_url, external_url, icon, extras, id))
+        execute_query(
+            'UPDATE apps SET name=?, category=?, description=?, internal_url=?, external_url=?, icon=?, '
+            'extras=? WHERE id=?',
+            (app_name, category, description, internal_url, external_url, icon, extras, id))
+
+        myapp = execute_query('SELECT id,name FROM apps where name=?', (app_name,), one=True)
+
+        if myapp['id'] > -1:
+            for tag in tags:
+                t = execute_query('SELECT id, tag FROM tags WHERE tag=?', (tag,), one=True)
+                execute_query('INSERT INTO app_tags(app_id, tag_id) VALUES(?, ?)', (myapp['id'], t['id']))
 
         return redirect(url_for('list'))
 
@@ -137,49 +147,46 @@ def get_icon_list():
             icons.append(ico)
     return icons
 
+
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     form = AppForm()
-    allApps = []
-    alltags = []
+    all_tags = []
     if form.validate_on_submit():
-        
-        appName = form.name.data
+
+        app_name = form.name.data
         category = form.category.data
         description = form.description.data
         internal_url = form.internal_url.data
         external_url = form.external_url.data
-        upload_file = form.icon.data
         tags = form.tags.data
         extras = form.extras.data
         icon = form.icon.data
 
-
-        allApps = execute_query('SELECT id, name, category, description, internal_url, external_url FROM apps')
-
         if len(tags) > 0:
             if ',' in tags:
-                alltags = tags.split(',')
-                for tag in alltags:
+                all_tags = tags.split(',')
+                for tag in all_tags:
                     execute_query('INSERT OR IGNORE INTO tags (tag) VALUES (?)', (tag.strip(),))
             else:
                 execute_query('INSERT OR IGNORE INTO tags (tag) VALUES (?)', (tags,))
-                allTags = tags.strip()
+                all_tags = tags.strip()
 
         execute_query('INSERT OR IGNORE INTO apps (name, category, description, internal_url, external_url, icon, extras)\
-            VALUES (?, ?, ?, ?, ?, ?, ?)', (appName.strip(), category, description, internal_url, external_url, icon, extras))
+            VALUES (?, ?, ?, ?, ?, ?, ?)',
+                      (app_name.strip(), category, description, internal_url, external_url, icon, extras))
 
-        myapp = execute_query('SELECT id,name FROM apps where name=?', (appName,), one=True)
+        myapp = execute_query('SELECT id,name FROM apps where name=?', (app_name,), one=True)
 
         if myapp['id'] > -1:
-            if len(alltags) > 0:
-                for tag in alltags:
+            if len(all_tags) > 0:
+                for tag in all_tags:
                     t = execute_query('SELECT id, tag FROM tags WHERE tag=?', (tag,), one=True)
                     execute_query('INSERT INTO app_tags(app_id, tag_id) VALUES(?, ?)', (myapp['id'], t['id']))
-            
+
         return redirect(url_for('index'))
-        
-    return render_template('add.html', form=form, title='App Form', items=alltags, icons=get_icon_list())
+
+    return render_template('add.html', form=form, title='App Form', items=all_tags, icons=get_icon_list())
 
 
 if __name__ == '__main__':
