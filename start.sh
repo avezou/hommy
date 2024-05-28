571d3525f39dbe8f931bf8ac2184fbe3ebe110b5
Avezou Petit Frere <github@apfware.com>
2024-05-28 11:50:52 -0400

    Update start.sh
    Create env if it doesn't exist
    
    Signed-off-by: Avezou Petit Frere <github@apfware.com>

--- start.sh
+++ start.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
+deactivate
+if [ ! -d ./env ]; then
+  python3 -m venv env
+fi
 source ./env/bin/activate
 pip install -r requirements.txt
 if ! test -f ./database.db; then
-  python init_db.py
+  ./env/bin/python init_db.py
 fi
-gunicorn -w $(nproc) --threads 2 --max-requests 2 app:app --bind 0.0.0.0:8000
+./env/bin/gunicorn -w $(nproc) --threads 2 --max-requests 2 app:app --bind 0.0.0.0:8000
 
 
